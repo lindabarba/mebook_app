92ec2550bc0ca295f4735552bed7320df8241eef
Linda Barba <lindabarba13@gmail.com>
2017-04-19 14:38:37 -0700

    add user & post models views routes controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    # @user = User.find(user_params)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def post
+  end
+
+private
+
+  # # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :image)
+  # end
+
+end
